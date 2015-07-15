7a8dc3f46adc5eb85653210b8a0e9319d4478006
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2015-07-15 22:28:53 +1200

    Added pop-up warning for delete product button

--- BlackBookPort/ViewProducts.cs
+++ BlackBookPort/ViewProducts.cs
@@ -22,6 +22,31 @@ namespace BlackBookPort
             this.Close();
         }
 
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var message = "Are you sure you want to delete that?";
+            var title = "Delete Product";
+            var result = MessageBox.Show(
+                message,                  // the message to show
+                title,                    // the title for the dialog box
+                MessageBoxButtons.YesNo,  // show two buttons: Yes and No
+                MessageBoxIcon.Question); // show a question mark icon
+
+            // the following can be handled as if/else statements as well
+            switch (result)
+            {
+                case DialogResult.Yes:   // Yes button pressed
+                    MessageBox.Show("Deleting product...");
+                    break;
+                case DialogResult.No:    // No button pressed
+                    MessageBox.Show("Whew!");
+                    break;
+                default:                 // Neither Yes nor No pressed (just in case)
+                    MessageBox.Show("What did you press?");
+                    break;
+            }
+        }
+
           
     }
 }
