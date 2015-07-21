b66e0396e034cc10f1413fba5d443083a9351a48
Isabel Francesca Baltazar <cheska.baltazar@gmail.com>
2015-07-21 23:07:40 +1200

    Added Customer Domain Object

---
+++ BlackBookPort/domain/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackBookPort.domain
+{
+    /** Customer Domain Object*/
+    class Customer
+    {
+        private String userName;
+        private String name;
+        private String creditCardDetails;
+        private String password;
+
+        public String Username { get { return userName; } set { this.userName = value; } }
+        public String Name { get { return name; } set { this.name = value; } }
+        
+        public void SetCreditCardDetails(String creditCardDetails) {
+            this.creditCardDetails = creditCardDetails;
+        }
+
+        public void SetPassword(String password) {
+            this.password = password;
+        }
+
+
+    }
+}
